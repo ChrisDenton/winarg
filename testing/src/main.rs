33d53c6fb850e021081e9e8ec3f7e1387e030c5f
Chris Denton <christophersdenton@gmail.com>
2021-07-27 17:13:33 +0100

    Initial commit

---
+++ testing/src/main.rs
@@ -0,0 +1,188 @@
+// Generates exhaustive test cases.
+// This was thrown together quite quickly to ensure the parser was correct/
+// It could definitely be improved.
+
+// Note this only generates the
+
+use std::{ffi::c_void, fs::File, os::windows::io::AsRawHandle, ptr::null_mut as null};
+
+fn main() {
+	let mut buffer = Io::new("output.txt");
+	let input: Vec<u16> = "\\a\" \t".encode_utf16().collect(); // 一 \t
+
+	perms(&input, 6, move |perm| {
+		run_args(perm, &mut buffer);
+	});
+}
+
+// Enumerate all permutations with repetitions and for all output lengths from 1 to `max_len`.
+// Don't ask me how this works, I typed it out in a single stream of consciousness.
+fn perms_iter<'a, T: Copy>(
+	input: &'a [T],
+	max_len: u32,
+) -> impl Iterator<Item = impl Iterator<Item = T> + 'a> {
+	(1..=max_len)
+		.flat_map(move |len| (0..input.len().pow(len)).zip(std::iter::repeat(len)))
+		.map(move |(mut n, j)| {
+			(0..j).map(move |_| {
+				let s = input[n % input.len()];
+				n /= input.len();
+				s
+			})
+		})
+}
+fn perms<F: FnMut(&mut [u16])>(input: &[u16], max_len: u16, mut f: F) {
+	let mut buffer = Vec::with_capacity((max_len + 1) as _);
+
+	for args in perms_iter(input, max_len as _) {
+		buffer.clear();
+		for unit in args {
+			buffer.push(unit);
+		}
+		buffer.push(0);
+		f(&mut buffer);
+	}
+}
+
+// Call `CreateProcessW` with the command line and write the output to `buffer`.
+// We can't use std::process::Command because it doesn't (yet) allow setting the
+// zeroth argument. Nightly does support raw_args for setting the others.
+fn run_args(args: &mut [u16], buffer: &mut Io) {
+	// args.exe
+	static NAME: &[u16] = &[
+		b'a' as _, b'r' as _, b'g' as _, b's' as _, b'.' as _, b'e' as _, b'x' as _, b'e' as _, 0,
+	];
+	unsafe {
+		let mut startup = STARTUPINFOW::new();
+		startup.dwFlags = 0x100;
+		startup.hStdOutput = buffer.write;
+
+		let mut info = PROCESS_INFORMATION::new();
+		let result = CreateProcessW(
+			NAME.as_ptr(),
+			args.as_mut_ptr(),
+			null(),
+			null(),
+			1,
+			0,
+			null(),
+			null(),
+			&startup,
+			&mut info,
+		);
+		assert_ne!(result, 0);
+		// Wait for the process to exit to avoid overlapping writes.
+		let result = WaitForSingleObject(info.hProcess, u32::MAX);
+		assert_ne!(result, u32::MAX);
+
+		let mut exit_code = 0;
+		let result = GetExitCodeProcess(info.hProcess, &mut exit_code);
+		assert_ne!(result, 0);
+		assert_eq!(exit_code, 0);
+
+		CloseHandle(info.hProcess);
+		CloseHandle(info.hThread);
+	}
+}
+
+// Some thing to write the output to. Could be a pipe but in this case I'm saving directly to a file so it can be read.
+struct Io {
+	write: usize,
+}
+impl Io {
+	fn new(name: &str) -> Self {
+		let f = File::create(name).unwrap();
+		let handle = f.as_raw_handle();
+		let mut output = 0;
+		unsafe {
+			DuplicateHandle(
+				GetCurrentProcess(),
+				handle as _,
+				GetCurrentProcess(),
+				&mut output,
+				0,
+				1,
+				2,
+			);
+		}
+		Self { write: output }
+	}
+}
+impl Drop for Io {
+	fn drop(&mut self) {
+		unsafe {
+			CloseHandle(self.write);
+		}
+	}
+}
+
+#[repr(C)]
+#[allow(nonstandard_style)]
+struct PROCESS_INFORMATION {
+	hProcess: usize,
+	hThread: usize,
+	dwProcessId: u32,
+	dwThreadId: u32,
+}
+impl PROCESS_INFORMATION {
+	fn new() -> Self {
+		unsafe { return std::mem::zeroed() }
+	}
+}
+#[repr(C)]
+#[allow(nonstandard_style)]
+struct STARTUPINFOW {
+	cb: u32,
+	lpReserved: *mut u16,
+	lpDesktop: *mut u16,
+	lpTitle: *mut u16,
+	dwX: u32,
+	dwY: u32,
+	dwXSize: u32,
+	dwYSize: u32,
+	dwXCountChars: u32,
+	dwYCountChars: u32,
+	dwFillAttribute: u32,
+	dwFlags: u32,
+	wShowWindow: u16,
+	cbReserved2: u16,
+	lpReserved2: *mut u8,
+	hStdInput: usize,
+	hStdOutput: usize,
+	hStdError: usize,
+}
+impl STARTUPINFOW {
+	fn new() -> Self {
+		let mut new: STARTUPINFOW = unsafe { std::mem::zeroed() };
+		new.cb = std::mem::size_of::<Self>() as _;
+		new
+	}
+}
+
+extern "system" {
+	fn CreateProcessW(
+		lpApplicationName: *const u16,
+		lpCommandLine: *mut u16,
+		lpProcessAttributes: *const c_void,
+		lpThreadAttributes: *const c_void,
+		bInheritHandles: i32,
+		dwCreationFlags: u32,
+		lpEnvironment: *const u16,
+		lpCurrentDirectory: *const u16,
+		lpStartupInfo: *const STARTUPINFOW,
+		lpProcessInformation: *mut PROCESS_INFORMATION,
+	) -> i32;
+	fn WaitForSingleObject(hHandle: usize, dwMilliseconds: u32) -> u32;
+	fn CloseHandle(hObject: usize) -> i32;
+	fn DuplicateHandle(
+		hSourceProcessHandle: usize,
+		hSourceHandle: usize,
+		hTargetProcessHandle: usize,
+		lpTargetHandle: *mut usize,
+		dwDesiredAccess: u32,
+		bInheritHandle: i32,
+		dwOptions: u32,
+	) -> i32;
+	fn GetCurrentProcess() -> usize;
+	fn GetExitCodeProcess(hProcess: usize, lpExitCode: *mut u32) -> i32;
+}
---
+++ testing/tests/exhaustive.rs
@@ -0,0 +1,89 @@
+use std::{
+	fs::File,
+	io::{self, BufRead},
+	sync::atomic::{AtomicPtr, Ordering},
+};
+
+// Again, this isn't very good and should probably be rewritten.
+// It does the job though (mostly).
+
+#[test]
+fn exhaustive() -> io::Result<()> {
+	let f = File::open("output.txt")?;
+	let reader = io::BufReader::new(f);
+	let mut lines = reader.lines();
+	let mut buffer = Vec::new();
+	let mut counter = 0_usize;
+	loop {
+		let cmdline = match lines.next() {
+			Some(line) => line?,
+			None => break,
+		};
+		if cmdline.len() > 10 {
+			break;
+		}
+		counter += 1;
+		let argc = match lines.next() {
+			Some(line) => line?.parse::<u16>().unwrap(),
+			None => break,
+		};
+		buffer.clear();
+		for _ in 0..argc {
+			let arg = match lines.next() {
+				Some(line) => line?,
+				None => break,
+			};
+			buffer.push(arg);
+		}
+		//
+		let cmd: Vec<u16> = cmdline.encode_utf16().chain(Some(0)).collect();
+		CMD.store(cmd.as_ptr() as _, std::sync::atomic::Ordering::Relaxed);
+
+		let mut counter = 0;
+		for arg in winarg::ArgsNative::from_env() {
+			assert_eq!(arg, buffer[counter].as_str());
+			counter += 1;
+		}
+		assert_eq!(counter, buffer.len());
+
+		let mut counter = 0;
+		let args: String = winarg::null_seperated_list().collect();
+		for arg in args.split('\0') {
+			assert_eq!(arg, buffer[counter], "{:?}", cmdline);
+			counter += 1;
+		}
+		assert_eq!(counter, buffer.len());
+
+		let mut parser = winarg::Parser();
+		// Skip the zeroth argument.
+		for t in &mut parser {
+			if t.is_next_arg() {
+				break;
+			}
+		}
+
+		// Collect the rest into a UTF-16 encoded vector.
+		let args: Vec<u16> = parser.map(|t| t.as_u16()).collect();
+		for (index, arg) in args.split(|&w| w == 0).enumerate() {
+			// `split` will produce a single empty argument if args is empty.
+			if index + 1 == buffer.len() {
+				assert!(arg.is_empty());
+			} else {
+				let arg = String::from_utf16_lossy(arg);
+				assert_eq!(arg, buffer[index + 1], "{:?}", cmdline);
+			}
+		}
+	}
+	println!("{}", counter);
+	Ok(())
+}
+
+// Replace `GetCommandLineW` with our own version.
+// WARNING: Never do this in real code.
+// Replacing system functions is massively unsafe and a recipe for disaster.
+// Especially so with a library that assumes the memory is 'static.
+static CMD: AtomicPtr<u16> = AtomicPtr::new(std::ptr::null_mut());
+#[no_mangle]
+extern "C" fn GetCommandLineW() -> *const u16 {
+	CMD.load(Ordering::Relaxed)
+}
